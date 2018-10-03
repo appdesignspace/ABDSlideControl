175af9d613ce8c84fb967c9da61d08a912734b38
appdesignspace <destin.makuyana@gmail.com>
2018-10-03 19:00:14 +0700

    Initial Commit

---
+++ ABDSliderControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ABDSliderControl
+//
+//  Created by Destin on 03/10/2018.
+//  Copyright © 2018 com.appsbydestin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
